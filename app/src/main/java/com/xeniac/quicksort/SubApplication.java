2366f3e7f631158f0d8a312159de306083815bda
WilliamGates99 <william.gates.3299@gmail.com>
2019-11-09 19:18:03 +0330

    Initial commit

---
+++ app/src/main/java/com/xeniac/quicksort/SubApplication.java
@@ -0,0 +1,11 @@
+package com.xeniac.quicksort;
+
+import android.app.Application;
+
+public class SubApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
