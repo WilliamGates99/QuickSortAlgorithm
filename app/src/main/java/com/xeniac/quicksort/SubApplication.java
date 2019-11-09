6461285f2840283698f4aa3778f85543f7768818
WilliamGates99 <william.gates.3299@gmail.com>
2019-11-09 21:39:59 +0330

    UI Improved

--- app/src/main/java/com/xeniac/quicksort/SubApplication.java
+++ app/src/main/java/com/xeniac/quicksort/SubApplication.java
@@ -2,10 +2,14 @@ package com.xeniac.quicksort;
 
 import android.app.Application;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class SubApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
