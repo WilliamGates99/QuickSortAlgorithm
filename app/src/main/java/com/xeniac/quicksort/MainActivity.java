54cfefa97cf7ab99bbc01ca4432c14db25a206fb
WilliamGates99 <william.gates.3299@gmail.com>
2019-11-10 19:16:17 +0330

    UI Completed

--- app/src/main/java/com/xeniac/quicksort/MainActivity.java
+++ app/src/main/java/com/xeniac/quicksort/MainActivity.java
@@ -2,19 +2,30 @@ package com.xeniac.quicksort;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.xeniac.quicksort.adapters.MainAdapter;
 import com.xeniac.quicksort.dataProviders.MainDataProvider;
 import com.xeniac.quicksort.models.DataItemMain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText mainET;
+    private TextView initialEmptyTV, sortedEmptyTV;
+
+    private RecyclerView initialRV, sortedRV;
+    private List<DataItemMain> dataItemMainList = MainDataProvider.dataItemMainList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +34,83 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-        mainRecyclerView();
+        mainInitializer();
+    }
+
+    private void mainInitializer() {
+        mainET = findViewById(R.id.ti_main_edit);
+        initialEmptyTV = findViewById(R.id.tv_main_initial_empty);
+        sortedEmptyTV = findViewById(R.id.tv_main_sorted_empty);
+
+        initialRV = findViewById(R.id.rv_main_initial);
+        sortedRV = findViewById(R.id.rv_main_sorted);
+
+        mainCondition();
+        editTextActionDone();
+    }
+
+    private void mainCondition() {
+        if (dataItemMainList.isEmpty()) {
+            initialRV.setVisibility(View.GONE);
+            sortedRV.setVisibility(View.GONE);
+
+            initialEmptyTV.setVisibility(View.VISIBLE);
+            sortedEmptyTV.setVisibility(View.VISIBLE);
+        } else {
+            initialEmptyTV.setVisibility(View.GONE);
+            sortedEmptyTV.setVisibility(View.VISIBLE);
+
+            initialRV.setVisibility(View.VISIBLE);
+            sortedRV.setVisibility(View.GONE);
+
+            mainRecyclerView();
+        }
     }
 
     private void mainRecyclerView() {
-        MainAdapter mainAdapter = new MainAdapter(this, MainDataProvider.dataItemMainList);
-        RecyclerView mainInitialRV = findViewById(R.id.rv_main_initial);
-        mainInitialRV.setAdapter(mainAdapter);
+        MainAdapter initialAdapter = new MainAdapter(this, dataItemMainList);
+        initialRV.setAdapter(initialAdapter);
+    }
 
-        RecyclerView mainSortedRV = findViewById(R.id.rv_main_sorted);
-        mainSortedRV.setAdapter(mainAdapter);
+    private void editTextActionDone() {
+        mainET.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                addOnClick(v);
+            }
+            return false;
+        });
+    }
+
+    public void addOnClick(View view) {
+        String input = Objects.requireNonNull(mainET.getText()).toString();
+
+        if (TextUtils.isEmpty(input)) {
+            mainET.requestFocus();
+            InputMethodManager methodManager =
+                    (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (methodManager != null) {
+                methodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        } else {
+            InputMethodManager methodManager =
+                    (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (methodManager != null) {
+                methodManager.hideSoftInputFromWindow(mainET.getWindowToken(), 0);
+            }
+            mainET.setText(null);
+            mainET.clearFocus();
+
+            dataItemMainList.add(new DataItemMain(Integer.parseInt(input)));
+            mainCondition();
+            sortedEmptyTV.setText(R.string.string_main_sorted_msg);
+        }
+    }
+
+    public void sortOnClick(View view) {
+        sortedEmptyTV.setVisibility(View.GONE);
+        sortedRV.setVisibility(View.VISIBLE);
+
+        MainAdapter sortedAdapter = new MainAdapter(this, dataItemMainList);
+        sortedRV.setAdapter(sortedAdapter);
     }
 }
--- app/src/main/java/com/xeniac/quicksort/dataProviders/MainDataProvider.java
+++ app/src/main/java/com/xeniac/quicksort/dataProviders/MainDataProvider.java
@@ -12,30 +12,6 @@ public class MainDataProvider {
     static {
         dataItemMainList = new ArrayList<>();
 
-        dataItemMainList.add(new DataItemMain(2));
-        dataItemMainList.add(new DataItemMain(2));
-        dataItemMainList.add(new DataItemMain(2));
-        dataItemMainList.add(new DataItemMain(2));
-        dataItemMainList.add(new DataItemMain(2));
-        dataItemMainList.add(new DataItemMain(2));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(22));
-        dataItemMainList.add(new DataItemMain(222));
-        dataItemMainList.add(new DataItemMain(222));
-        dataItemMainList.add(new DataItemMain(222));
-        dataItemMainList.add(new DataItemMain(222));
-        dataItemMainList.add(new DataItemMain(222));
-        dataItemMainList.add(new DataItemMain(2222));
-        dataItemMainList.add(new DataItemMain(2222));
-        dataItemMainList.add(new DataItemMain(2222));
-        dataItemMainList.add(new DataItemMain(2222));
-        dataItemMainList.add(new DataItemMain(2222));
-        dataItemMainList.add(new DataItemMain(2222));
     }
 
     private static void addItem(DataItemMain item) {
